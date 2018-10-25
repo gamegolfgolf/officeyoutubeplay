0904ccf0b66ea8897c8f3f0d5b24ce70b00c2844
GamEGolF <theeramet@builk.com>
2018-10-25 15:05:44 +0700

    test deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDVFTPo7qaEO7sgbzkgwHXofvYxx6HjtMg",
-    authDomain: "gamegolf-prod.firebaseapp.com",
-    databaseURL: "https://gamegolf-prod.firebaseio.com",
-    projectId: "gamegolf-prod",
-    storageBucket: "gamegolf-prod.appspot.com",
-    messagingSenderId: "560174108877"
+    apiKey: "AIzaSyCrE89TaFGYsZ7S1SApOKwVxXymZsJhxiQ",
+    authDomain: "test-new-project-gg.firebaseapp.com",
+    databaseURL: "https://test-new-project-gg.firebaseio.com",
+    projectId: "test-new-project-gg",
+    storageBucket: "test-new-project-gg.appspot.com",
+    messagingSenderId: "381925540763"
   }
 };
