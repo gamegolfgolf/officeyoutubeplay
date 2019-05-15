31abd68db21c4113a21c82dadd44cc2f4dce40c7
GamEGolF <theeramet@builk.com>
2019-05-15 11:00:51 +0700

    change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDP7P98bRPZt5X4YYEwF1W7J1VOMGA8CRQ",
-    authDomain: "gamegolf-74043.firebaseapp.com",
-    databaseURL: "https://gamegolf-74043.firebaseio.com",
-    projectId: "gamegolf-74043",
-    storageBucket: "gamegolf-74043.appspot.com",
-    messagingSenderId: "99558072242"
+    apiKey: "AIzaSyDj1qnZbImRbvm3O-akcxorwcvlAK4kdoM",
+    authDomain: "gamegolf-a117d.firebaseapp.com",
+    databaseURL: "https://gamegolf-a117d.firebaseio.com",
+    projectId: "gamegolf-a117d",
+    storageBucket: "gamegolf-a117d.appspot.com",
+    messagingSenderId: "671884081023"
   }
 };
 
