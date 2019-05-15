12d846a9bb1a3bfdd5b300faf26fdedc2f92f871
GamEGolF <theeramet@builk.com>
2019-05-15 10:55:26 +0700

    change evn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyDj1qnZbImRbvm3O-akcxorwcvlAK4kdoM",
     authDomain: "gamegolf-a117d.firebaseapp.com",
-    databaseURL: "https://gamegolf-a117d.firebaseio.com/",
+    databaseURL: "https://gamegolf-a117d.firebaseio.com",
     projectId: "gamegolf-a117d",
     storageBucket: "gamegolf-a117d.appspot.com",
     messagingSenderId: "671884081023"
